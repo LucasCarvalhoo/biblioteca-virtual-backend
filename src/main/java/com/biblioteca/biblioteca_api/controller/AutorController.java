e6d14a26f65a50b003bb67781f32d7d5fe0b3253
Lucas Carvalho <lucas.carvalho7933@gmail.com>
2025-07-03 19:52:13 -0300

    chore: ajustar rotas

--- src/main/java/com/biblioteca/biblioteca_api/controller/AutorController.java
+++ src/main/java/com/biblioteca/biblioteca_api/controller/AutorController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/autor")
+@RequestMapping("/api/autores")
 public class AutorController {
 
     @Autowired
--- src/main/java/com/biblioteca/biblioteca_api/controller/CategoriaController.java
+++ src/main/java/com/biblioteca/biblioteca_api/controller/CategoriaController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/categoria")
+@RequestMapping("/api/categorias")
 public class CategoriaController {
 
     @Autowired
